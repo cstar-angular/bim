673c602c2b8b9118aea9bfb7e202cc0adf61e759
manzar <czechmanzaremail1@gmail.com>
2018-11-29 11:54:18 -0700

    auth and avartar text

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from '../_services/auth.service';
+import { UserProfile } from '../user/profile';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-settings',
@@ -8,8 +13,29 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<SettingsComponent>) {
-    
+  activeTab = 'Profile';
+  authUser: firebase.User;
+  uid;
+  
+  userProfile = new UserProfile();
+
+  constructor(
+    public dialogRef: MatDialogRef<SettingsComponent>,
+    private auth: AngularFireAuth ,
+    private authService: AuthService
+  ) {
+    this.authUser = auth.auth.currentUser;
+    this.userProfile.email = this.authUser.email;
+    // this.userProfile.phone = this.authUser.phoneNumber;
+    this.userProfile.avatar = this.authUser.photoURL;
+    this.userProfile.uid = this.authUser.uid
+    this.uid = this.authUser.uid;
+
+    this.authService.getUserById(this.uid).valueChanges().subscribe(data => {
+      this.userProfile.name = data['name'];
+      this.userProfile.company_name = data['company_name'];
+      this.userProfile.phone = data['phone'];
+    });
   }
 
   onNoClick(): void {
@@ -18,4 +44,14 @@ export class SettingsComponent implements OnInit {
   ngOnInit() {
   }
 
+  updateProfile() {
+    this.authUser.updateEmail(this.userProfile.email).then(function() {
+      console.log('success');
+    }).catch(function(error) {
+      // An error happened.
+      console.log(error);
+    });
+
+    this.authService.updateUserById(this.userProfile.uid, this.userProfile);
+  }
 }
