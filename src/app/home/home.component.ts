365b76362d1c387145392fbcc31be3aa51df8492
stardev0220 <mrg8406@gmail.com>
2018-12-14 03:43:34 -0500

    update project count by membership

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,33 +21,72 @@ export class HomeComponent implements OnInit {
     private projectService: ProjectprofileService,
     private databaseService: DatabaseService,
     private authService: AuthService
-  ) { }
+  ) { 
+    if (this.currentUser) {
+      this.databaseService.getRowDetails('/users', this.currentUser.uid).valueChanges().subscribe(data => {
+        this.currentUser = data;
+      });
+    }
+  }
 
   ngOnInit() {
+
+    // Decide the project count by membership
+    var maxCount = 5;
+    if (this.currentUser && this.currentUser.membership) {
+      maxCount = this.currentUser.membership.type;
+    }
+
     this.projectService.getProjectsList().snapshotChanges().pipe(
       map(changes => 
         changes.map(c => ({ key: c.payload.key, ...c.payload.val() })).filter(proj => proj.created_by == this.currentUser.uid)
       )
     ).subscribe(data => {
       if(data) {
-        this.projects = data;
-      }
-    });
+        
+        // Check the count of the projects
+        if (data.length > maxCount) {
+          var projects = [];
 
-    this.databaseService.getLists('/user-project').snapshotChanges().pipe(
-      map(changes => 
-        changes.map(c => ({ key: c.payload.key, ...c.payload.val() })).filter(data => data.userid == this.currentUser.uid)
-      )
-    ).subscribe(data => {
-      if (data) {
-        this.invitedProjects = [];
-        data.forEach(item => {
-          this.databaseService.getRowDetails('/projects', item.projectid).valueChanges().subscribe(project => {
-            project.key = item.projectid;
-            this.invitedProjects.push(project);
+          data.forEach(item => {
+            
+            if (projects.length < maxCount) {
+              projects.push(item);
+            }
+
           });
-          
-        });
+
+          this.projects = projects;
+
+        } else {
+
+          this.projects = data;
+          var moreCount = maxCount - this.projects.length;
+
+          // Add the invited projects
+          this.databaseService.getLists('/user-project').snapshotChanges().pipe(
+            map(changes => 
+              changes.map(c => ({ key: c.payload.key, ...c.payload.val() })).filter(data => data.userid == this.currentUser.uid)
+            )
+          ).subscribe(data => {
+            if (data) {
+              this.invitedProjects = [];
+              data.forEach(item => {
+                this.databaseService.getRowDetails('/projects', item.projectid).valueChanges().subscribe(project => {
+                  project.key = item.projectid;
+
+                  if (this.invitedProjects.length < moreCount) {
+                    this.invitedProjects.push(project);
+                  }
+                  
+                });
+                
+              });
+            }
+          });
+
+        }
+
       }
     });
     
